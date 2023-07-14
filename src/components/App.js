e6129997e1c1fd69fb0c2acf96d9ace1b7a7fe0d
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-07-14 17:40:55 +0000

    counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [add, setAdd] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {add} times</p>
+        <button onClick={()=>setAdd(add+1)}>Click me</button>
     </div>
   )
 }
